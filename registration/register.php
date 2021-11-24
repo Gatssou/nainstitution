b917f0e46aca51627dc1c84aef1d9d47624838c0
Gatssou <juliendu62155@hotmail.FR>
2021-11-24 10:58:47 +0100

    hg
    hg

--- registration/register.php
+++ registration/register.php
@@ -32,15 +32,15 @@ if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])){
 	// récupérer le nom d'utilisateur et supprimer les antislashes ajoutés par le formulaire
 	$username = stripslashes($_REQUEST['username']);
 	$username = mysqli_real_escape_string($conn, $username); 
-	// récupérer l'email et supprimer les antislashes ajoutés par le formulaire
+
 	$email = stripslashes($_REQUEST['email']);
 	$email = mysqli_real_escape_string($conn, $email);
-	// récupérer le mot de passe et supprimer les antislashes ajoutés par le formulaire
+
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($conn, $password);
 	//requéte SQL + mot de passe crypté
     $query = "INSERT into `users` (username, email, password)
-              VALUES ('$username', '$email', '".hash('sha256', $password)."')";
+              VALUES ('$username', '$email', '$password')";
 	// Exécute la requête sur la base de données
     $res = mysqli_query($conn, $query);
     if($res){
