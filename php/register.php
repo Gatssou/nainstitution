10ee88cc7eae502cf63831eebbf4f37c09cf071d
Gatssou <juliendu62155@hotmail.FR>
2021-11-24 13:52:23 +0100

    fdfdf
    dfdfd

--- php/register.php
+++ php/register.php
@@ -51,31 +51,7 @@ if($recupUser->rowCount() > 0){
 </section>
 <div id="root"></div>
 <div class="hoo"><span class="timer"></span></div>
-<?php
-require('config.php');
-if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])){
-	// récupérer le nom d'utilisateur et supprimer les antislashes ajoutés par le formulaire
-	$username = stripslashes($_REQUEST['username']);
-	$username = mysqli_real_escape_string($conn, $username); 
-	// récupérer l'email et supprimer les antislashes ajoutés par le formulaire
-	$email = stripslashes($_REQUEST['email']);
-	$email = mysqli_real_escape_string($conn, $email);
-	// récupérer le mot de passe et supprimer les antislashes ajoutés par le formulaire
-	$password = stripslashes($_REQUEST['password']);
-	$password = mysqli_real_escape_string($conn, $password);
-	//requéte SQL + mot de passe crypté
-    $query = "INSERT into `users` (username, email, password)
-              VALUES ('$username', '$email', '".hash('sha256', $password)."')";
-	// Exécute la requête sur la base de données
-    $res = mysqli_query($conn, $query);
-    if($res){
-       echo "<div class='success'>
-             <h3>Vous êtes inscrit avec succès.</h3>
-             <p>Cliquez ici pour vous <a href='login.php'>connecter</a></p>
-			 </div>";
-    }
-}else{
-?>
+
 <section class="zindex">
 	
 	<form class="labin" action="" method="post">
@@ -105,7 +81,7 @@ if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])){
 		<p class="box-register">Déjà inscrit? <br> <a class="dec" href="login.php">Connectez-vous ici</a></p>
 	</form>
 </section>
-<?php } ?>
+
 </body>
 <script src="../js/js stock/jquery-ui-1.13.0/jquery-ui.js"></script>
 <script src="../js/login.js"></script>
