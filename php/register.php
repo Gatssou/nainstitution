924df79ae95cb854d94211296c0c4de2de24caf4
Gatssou <juliendu62155@hotmail.FR>
2021-11-24 17:03:18 +0100

    gf
    gf

--- php/register.php
+++ php/register.php
@@ -27,28 +27,63 @@
 </section>
 <div id="root"></div>
 <div class="hoo"><span class="timer"></span></div>
-<?php
-require('config.php');
-if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])){
-	// récupérer le nom d'utilisateur et supprimer les antislashes ajoutés par le formulaire
-	
-	$username = htmlspecialchars($_POST['username']); 
-	$email = htmlspecialchars($_POST['email']); 
-	$password = htmlspecialchars($_POST['password']); 
-	
-	//requéte SQL + mot de passe crypté
-    $query = "INSERT into `users` (username, email, password)
-              VALUES ('$username', '$email', '".hash('sha256', $password)."')";
-	// Exécute la requête sur la base de données
-    $res = mysqli_query($conn, $query);
-    if($res){
-       echo "<div class='success'>
-             <h3>Vous êtes inscrit avec succès.</h3>
-             <p>Cliquez ici pour vous <a href='login.php'>connecter</a></p>
-			 </div>";
+<?php 
+   $pdo= new PDO("mysql:host=localhost;dbname=nains", 'root');
+    // Si les variables existent et qu'elles ne sont pas vides
+    if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['password_retype']))
+    {
+        // Patch XSS
+        $username = htmlspecialchars($_POST['username']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+        $password_retype = htmlspecialchars($_POST['password_retype']);
+
+        // On vérifie si l'utilisateur existe
+        $check = $pdo->prepare('SELECT username, email, password FROM users WHERE email = ?');
+        $check->execute(array($email));
+        $data = $check->fetch();
+        $row = $check->rowCount();
+
+        
+        // Si la requete renvoie un 0 alors l'utilisateur n'existe pas 
+        if($row == 0)
+        { 
+           
+            if(strlen($username) <= 100)
+            { // On verifie que la longueur du pseudo <= 100
+                if(strlen($email) <= 100)
+                { // On verifie que la longueur du mail <= 100
+                    if(filter_var($email, FILTER_VALIDATE_EMAIL))
+                    { // Si l'email est de la bonne forme
+                        if($password == $password_retype)
+                        { // si les deux mdp saisis sont bon
+
+                            // On hash le mot de passe avec Bcrypt, via un coût de 12
+                        
+                            $password = hash('sha256', $password);
+                            
+                            // On stock l'adresse IP
+                            $ip = $_SERVER['REMOTE_ADDR']; 
+
+                            // On insère dans la base de données
+                            $insert = $pdo->prepare('INSERT INTO users (username, email, password, ip, token) VALUES(:username, :email, :password, :ip, :token)');
+                            $insert->execute(array(
+                                'username' => $username,
+                                'email' => $email,
+                                'password' => $password,
+                                'ip' => $ip,
+                                'token' => bin2hex(openssl_random_pseudo_bytes(64))
+                            ));
+                            // On redirige avec le message de succès
+                            header('Location:login.php?reg_err=success');
+                            die();
+                        }else{ header('Location: inscription.php?reg_err=password'); die();}
+                    }else{ header('Location: inscription.php?reg_err=email'); die();}
+                }else{ header('Location: inscription.php?reg_err=email_length'); die();}
+            }else{ header('Location: inscription.php?reg_err=username_length'); die();}
+        }else{ echo 'vous êtes deja inscrit' ; die(); }
     }
-}else{
-?>
+	?>
 <section class="zindex">
 	
 	<form class="labin" action="" method="post">
@@ -78,7 +113,7 @@ if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])){
 		<p class="box-register">Déjà inscrit? <br> <a class="dec" href="login.php">Connectez-vous ici</a></p>
 	</form>
 </section>
-<?php } ?>
+
 </body>
 <script src="../js/js stock/jquery-ui-1.13.0/jquery-ui.js"></script>
 <script src="../js/login.js"></script>
