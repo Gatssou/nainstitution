391301bada1065984bb49339bff5aec6ca36ce17
Arecin0L <betrancourt.lonicera@gmail.com>
2021-11-29 13:41:16 +0100

    test

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,6 @@
 </body>
-<script src="../js/js stock/jquery-ui-1.13.0/jquery-ui.js"></script>
-<script src="../js/login.js"></script>
-<script src="../js/index.js"></script>
+<script src="/../nainstitution/js/stock/jquery-3.6.0.min.js"></script>
+<script src="/../nainstitution/js/stock/jquery-ui-1.13.0/jquery-ui.js"></script>
+<script src="/../nainstitution/js/login.js"></script>
+<script src="/../nainstitution/js/index.js"></script>
 </html>
