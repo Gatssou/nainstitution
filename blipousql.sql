0970d7cfa30b0fc325c1a3e7b9636f4f655584d9
Arecin0L <betrancourt.lonicera@gmail.com>
2021-12-01 09:12:04 +0100

    blipou

--- blipousql.sql
+++ blipousql.sql
@@ -5,7 +5,7 @@
 -- HeidiSQL Version:             11.2.0.6213
 -- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT  */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
