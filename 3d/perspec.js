6ede7205b29bff9bde84d1aa739104a019829374
Arecin0L <betrancourt.lonicera@gmail.com>
2021-12-03 16:25:23 +0100

    line

--- 3d/perspec.js
+++ 3d/perspec.js
@@ -1,22 +1,86 @@
-const scene = new THREE.Scene()
-const renderer = new THREE.WebGLRenderer()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+/*
+Fonctionne mais prend toute la ligne et pas x et y
+$(document).ready(function(){
+    $("h2").click(function(){
+        let line = $("#line");
+        line.animate({top: '15vh'}, 3000);
+        line.animate({left: '15vh'}, 3000);
+        line.animate({top: '0vh'}, 3000);
+        line.animate({left: '0vh'}, 3000);
+        
+    });
+    
+});
+*/
 
-const geometry = new THREE.CylinderGeometry(5, 5, 20, 32)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
-const cylinder = new THREE.Mesh(geometry, material)
 
-scene.add(cylinder)
-camera.position.z = 20
 
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-                          
-function animate() {
-    cylinder.rotation.x += 0.01
-    cylinder.rotation.y += 0.01
-    renderer.render(scene, camera)
-    requestAnimationFrame(animate)
-}
+/*
+    const ctx = document.getElementById("canva").getContext("2d");
+    
+    let start = {x:150 , y:10 };
+    let cp1 =   { x: 230,   y: 30  };
+    let cp2 =   { x: 150,   y: 80  };
+    let end = {x:250 , y:100 };
 
-animate()
+    ctx.beginPath();
+    ctx.moveTo(start.x, end.y);
+    ctx.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, end.x, end.y);
+    ctx.stroke();
+
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.arc(start.x, start.y, 5, 0, 2 * Math.PI);
+    ctx.arc(end.x, end.y, 5, 0, 2 * Math.PI);
+    ctx.fill();
+    */
+    /*
+    ctx.lineWidth = "2";
+    ctx.strokeStyle = "white";
+    ctx.moveTo(150, 0);
+    ctx.lineTo(150, 150);
+    ctx.stroke();*/
+
+
+  /*  function gameLoop () {
+        window.requestAnimationFrame(gameLoop);
+        renderLine();
+      
+      }
+      let counter = 0; 
+      let old_position = {x: 0, y: 0};
+      let new_position = {x: 0, y: 0}; 
+      let width = 10;
+      let height = 10;
+
+      function renderLine(){
+       context.clearRect(old_position.x, old_position.y, width, height)
+       new_position = {x: 100, y: 200};  
+       old_position = new_position;
+      
+      }
+*/
+
+let ctx = document.getElementById("canva").getContext("2d");
+
+let start = {x:380 , y:10};
+let cp1 = {x: 200, y: 400};
+let cp2 = {x: 100, y: -200};
+let end = {x:400 , y:200};
+
+    ctx.lineWidth = "2";
+    ctx.strokeStyle = "white";
+    ctx.moveTo(start.x, start.y);
+    ctx.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, end.x, end.y);
+    ctx.stroke();
+
+let start2 = {x:400 , y:200};
+let cp3 = {x: 650, y: 500};
+let cp4 = {x: 700, y: 600};
+let end2 = {x:380 , y:900};
+
+    ctx.lineWidth = "2";
+    ctx.strokeStyle = "white";
+    ctx.moveTo(start2.x, start2.y);
+    ctx.bezierCurveTo(cp3.x, cp3.y, cp4.x, cp4.y, end2.x, end2.y);
+    ctx.stroke();
---
+++ 3d/perspec.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./perspec.css">
+    <title>Document</title>
+</head>
+<body id="scrollbody">
+    
+<div class="disp">
+    <h2>Line test js</h2>
+    <p>test</p>
+   <!-- <div id="line"></div>  --> 
+    <canvas id="canva" class="canva" width="760px" height="1000px"></canvas>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+<script src="../js/stock/jquery-3.6.0.min.js"></script>
+<script src="../js/stock/jquery-ui-1.13.0/jquery-ui.js"></script>
+<script src="./perspec.js"></script>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
