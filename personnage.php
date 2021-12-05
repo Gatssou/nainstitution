02564b54d09f03676d5342676adbb4640aec7df4
Gatssou <87131331+Gatssou@users.noreply.github.com>
2021-12-05 12:53:07 +0100

    fdfd
    fd

--- personnage.php
+++ personnage.php
@@ -13,6 +13,9 @@ logged_only();
     <title>Document</title>
 </head>
 <body>
+    <section>
+        <h2></h2>
+    </section>
     <div class="contien">
     <div class="menup active">
         <div class="persos"> 
@@ -70,21 +73,36 @@ logged_only();
     <footer>
  <a href="./include/disconnect.php">Se déconnecter</a>
  </footer>
+ 
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script type="text/javascript">
       $(document).on("click", ".menup", function () {
         $(this).addClass("active").siblings().removeClass("active");
       });
     </script>
-    </body>
+   
+
 <script>
-  $(window).scroll(function(){
-    let scrollPos = $(this).scrollTop() / 20;
-   // console.log(scrollPos);
-   $('body').css({
-     'background-size' : 120 + scrollPos + '%'
-   })
-  })
+    function changeBg(){
+const images = [
+'url("./img/imgbg/208702.jpg")',
+'url("./img/imgbg/584631.jpg")',
+'url("./img/imgbg/692298.jpg")',
+'url("./img/imgbg/ambiance.jpg")',
+'url("./img/imgbg/cimetière.png")',
+'url("./img/imgbg/FF.PNG")',
+'url("./img/imgbg/forêts.jpg.PNG")',
+'url("./img/imgbg/lapin8.jpg")',
+'url("./img/imgbg/ouff.jpg")',
+'url("./img/imgbg/village.jpg")',
+
+]
+        const section = document.querySelector('section')
+        const bg = images[Math.floor(Math.random() * images.length)
+    ];
+    section.style.backgroundImage = bg;
+    }
+    setInterval(changeBg, 3000)
 </script>
 </body>
 </html>
