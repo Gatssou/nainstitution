bcd5d88b7d52e6615c858fe85782dcede13b77af
Gatssou <87131331+Gatssou@users.noreply.github.com>
2021-12-08 20:51:31 +0100

    tr
    tr

--- include/inscription_merci.php
+++ include/inscription_merci.php
@@ -6,22 +6,41 @@ if(isset($_GET['conftoken'])){
     $conftoken = urldecode($conftoken);
     $conftoken = base64_decode($conftoken);
 
-    $check = $bdd->prepare('SELECT * FROM logte WHERE conftoken = ?');
-    $check->execute(array($conftoken));
+    $check = $pdo->prepare("SELECT * FROM `Membre` WHERE `email` LIKE '$mail' ");
+    $check->execute(array($mail));
     $data = $check->fetch();
     $row = $check->rowCount();
 
     if($row == NULL){
         if($data['roletype'] == NULL){
-            $update = $bdd->prepare('UPDATE logtest SET roletype = 1 WHERE conftoken = ?');
+            $update = $pdo->prepare('UPDATE logtest SET roletype = 1 WHERE conftoken = ?');
             echo "Compte verifie avec succes !";
         }else{
             echo "Compte deja verifie";
         }
-    }else{
-        echo "Compte existe pas";
     }
-}else{
-    echo "Mauvais lien";
 }
 ?>
+  if(isset($_POST['email']))
+{
+
+    try {
+        $ch = $pdo->prepare('SELECT email FROM logtest WHERE email = ?');
+        $ch->bindParam(1, $_POST['email']); 
+        $ch->execute();
+        while($row = $ch->fetch(PDO::FETCH_ASSOC)) {
+
+        }
+    }
+    catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+
+if($ch->rowCount() > 0){
+    echo "The record exists!";
+} else {
+    echo "The record is non-existant.";
+}
+
+
+}
