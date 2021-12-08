6f7d5ff9d5b40665f61bba0c6c67b858a02d009a
Gatssou <juliendu62155@hotmail.FR>
2021-12-08 16:32:38 +0100

    JHJ
    JHJH

--- include/inscript.php
+++ include/inscript.php
@@ -29,6 +29,7 @@ if(!empty($_POST['pseudo']) && !empty($_POST['email'])){
             header('location:../insc.php?reg_err=1');
         }
     }else{
+<<<<<<< Updated upstream
         if(!empty($_POST['mdp']) && !empty($_POST['mdpconf']) && preg_match('#(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[@!?*$.+-]).{6,18}#', $_POST['mdp']) && preg_match('#(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[@!?*$.+-]).{6,18}#', $_POST['mdpconf']) && $_POST['mdp'] === $_POST['mdpconf'])
 
 {
@@ -68,15 +69,65 @@ if(!empty($_POST) && !empty($hashed) && !empty($usname) && !empty($mail)){
         } 
     }
    
+=======
+        if(!empty($_POST['mdp']) && !empty($_POST['mdpconf']) && preg_match('#(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[@!?*$.+-]).{6,18}#', $_POST['mdp']) && preg_match('#(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[@!?*$.+-]).{6,18}#', $_POST['mdpconf']) && $_POST['mdp'] === $_POST['mdpconf']){
+            $pass = $_POST["mdp"];
+            $hashed = password_hash($pass, PASSWORD_BCRYPT);
+        }
+            if(!empty($_POST) && !empty($hashed) && !empty($usname) && !empty($mail)){
+                try{
+                    require_once './bdd.php';
+                    $req = $pdo->prepare("INSERT INTO logtest(username, password, email, conftoken) VALUES(:username, :password, :email, :conftoken)");
+                    $tok = token(60);
+                    $req->bindParam(':username', $usname);
+                    $req->bindParam(':password', $hashed);
+                    $req->bindParam(':email', $mail);
+                    $req->bindParam(':conftoken', $tok);
+                    $req->execute();
+
+                }catch(PDOException $e){
+                    echo "Erreur : " . $e;
+
+                }finally{
+                    $usid = $pdo->lastInsertId();
+                    $pdo = null;
+                    echo "Jean Bulbe";
+                    header("location:./confirm.php?id=" .$usid . "&token=" . $tok);
+        
+      //  header("location:./confirm.php");
+
+                }
+            }
+        else{
+       // header("location:../login.php?reg_succes=password");
+        } 
+    }
+
+  }  else{
+            header('location:../insc.php?reg_err=2');
+        }
+    }  
+ 
+>>>>>>> Stashed changes
     }else{
     echo 'Remplir les champs';
     }
+<<<<<<< Updated upstream
 }else{
     //header("location:../insc.php");
     echo "données";
 }
 
 
+=======
+}
+
+else{
+    //header("location:../insc.php");
+    echo "données";
+}
+    
+>>>>>>> Stashed changes
 
 ?>
 
