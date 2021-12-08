d886ccc4de6dd3313a6ebd2017aa0eeef534e945
val619 <87168643+val619@users.noreply.github.com>
2021-12-08 16:29:45 +0100

    git

--- include/inscript.php
+++ include/inscript.php
@@ -17,8 +17,11 @@ $mail = cleandata($_POST['email']);
 if(!empty($_POST['pseudo']) && !empty($_POST['email'])){
     try{
         require './bdd.php';
-        $check = $pdo->query("SELECT username, email FROM logtest where email=? ");
-        $data = $check->fetch(PDO::FETCH_OBJ);      
+        $check = $pdo->query("SELECT username, email FROM logtest where email = ?");
+        $data = $check->fetch(PDO::FETCH_OBJ);    
+        $req->bindParam(':username', $usname);
+        $req->bindParam(':email', $mail);
+        
     }catch(PDOException $e){
         echo 'Erreur : ' . $e;
     }finally{
@@ -26,49 +29,53 @@ if(!empty($_POST['pseudo']) && !empty($_POST['email'])){
             header('location:../insc.php?reg_err=1');
         }
     }else{
-        if(!empty($_POST['mdp']) && !empty($_POST['mdpconf']) && preg_match('#(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[@!?*$.+-]).{6,18}#', $_POST['mdp']) && preg_match('#(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[@!?*$.+-]).{6,18}#', $_POST['mdpconf']) && $_POST['mdp'] === $_POST['mdpconf']){
-            $pass = $_POST["mdp"];
-            $hashed = password_hash($pass, PASSWORD_BCRYPT);
+        if(!empty($_POST['mdp']) && !empty($_POST['mdpconf']) && preg_match('#(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[@!?*$.+-]).{6,18}#', $_POST['mdp']) && preg_match('#(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[@!?*$.+-]).{6,18}#', $_POST['mdpconf']) && $_POST['mdp'] === $_POST['mdpconf'])
 
-            if(!empty($_POST) && !empty($hashed) && !empty($usname) && !empty($mail)){
-                try{
-                    require_once './bdd.php';
-                    $req = $pdo->prepare("INSERT INTO logtest(username, password, email, conftoken) VALUES(:username, :password, :email, :conftoken)");
-                    $tok = token(60);
-                    $req->bindParam(':username', $usname);
-                    $req->bindParam(':password', $hashed);
-                    $req->bindParam(':email', $mail);
-                    $req->bindParam(':conftoken', $tok);
-                    $req->execute();
+{
+$pass = $_POST["mdp"];
+$hashed = password_hash($pass, PASSWORD_BCRYPT);
 
-                }catch(PDOException $e){
-                    echo "Erreur : " . $e;
+}
+else{
+    header('location:../insc.php?reg_err=2');
+}
 
-                }finally{
-                    $usid = $pdo->lastInsertId();
-                    $pdo = null;
-                    echo "Jean Bulbe";
-                    header("location:./confirm.php?id=" .$usid . "&token=" . $tok);
+if(!empty($_POST) && !empty($hashed) && !empty($usname) && !empty($mail)){
+    try{
+        require_once './bdd.php';
+        $req = $pdo->prepare("INSERT INTO logtest(username, password, email, conftoken) VALUES(:username, :password, :email, :conftoken)");
+        $tok = token(60);
+        $req->bindParam(':username', $usname);
+        $req->bindParam(':password', $hashed);
+        $req->bindParam(':email', $mail);
+        $req->bindParam(':conftoken', $tok);
+        $req->execute();
+
+    }catch(PDOException $e){
+        echo "Erreur : " . $e;
+
+    }finally{
+        $usid = $pdo->lastInsertId();
+        $pdo = null;
+        echo "Jean Bulbe";
+        header("location:./confirm.php?id=" .$usid . "&token=" . $tok);
         
       //  header("location:./confirm.php");
 
-                }
-            }
+    }
         }else{
        // header("location:../login.php?reg_succes=password");
         } 
     }
-
-        else{
-            header('location:../insc.php?reg_err=2');
-        }   
+   
     }else{
-        echo 'Remplir les champs';
+    echo 'Remplir les champs';
     }
 }else{
     //header("location:../insc.php");
     echo "données";
 }
+
 
 
 ?>
