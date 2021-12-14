878528ef0d5542309529647cced28bff4c2bdefa
Gatssou <87131331+Gatssou@users.noreply.github.com>
2021-12-14 19:22:42 +0100

    jhjh
    jhjh

--- include/admin/connexion.php
+++ include/admin/connexion.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require '../bdd.php';
-
+/* à l'activation du bouton valider on confirme les informations suivantes ce qui entraîne l'erreur ou la connexion avec une définition déjà donnée pseudo mdp */
 if(isset($_POST['valider'])) {
      if(!empty($_POST['pseudo']) AND !empty($_POST['mdp'])){
 $pseudo_par_defaut = "Gudule";
@@ -14,14 +14,10 @@ $mdp_par_defaut = "A15H17luma";
                  header('location: indexad.php');
 
              }else{
-                 echo "votre mot de pass est incorrecte";
+                header('location:connexion.php?log_err=1');
              }
- }else{
-     echo "veuillez compléter tous les champs...";
- 
-     }
+ }
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,39 +25,133 @@ $mdp_par_defaut = "A15H17luma";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+  
     <title>Document</title>
 </head>
 <body>
     <style>
-        body{
-            background-color: black;
-            text-align: center;
-            justify-content:center;
-            display: flex;
-      
+     
+        *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body{
+   transition: 0.5s;
+
 
         }
+
+body.active{
+    background: black; 
+}
+
+#toggle{
+    position: relative;
+    display: block;
+    width: 20px;
+    height: 20px;
+    border-radius: 16px;
+    background: white;
+    transition: 0.5s;
+    cursor: pointer;
+}
+#toggle.active{
+    background: black;
+    color: red;
+}
+#toggle .inducator{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 20px;
+    height: 20px;
+    background: linear-gradient(to bottom, #444,#222);
+    border-radius: 50%;
+    transform: scale(0.9);
+    transition: 0.5s;
+}
+#toggle.active .inducator{
+    left:199vh;
+    background: grey;
+    box-shadow: 0 8px 20px rgba(0,0,0,0.1),
+                inset 0 4px 4px rgba(255,255,255,1),
+                inset 0 -4px 4px rgba(255,255,255,1);
+}
+       
        input{
          background: white;
         }
         form{
+           
+            width: 40vw;
             margin-top:25%;
+            margin-left: 35%;
+            border: 1px white ridge;
+            border-radius: 12px  5px 5px 12px;
+            box-shadow: 10px  1px 1px 1px white;
         }
         .congrat{
             color: white;
         }
-
+        .motdepass{
+            background-color: white;
+            border-radius: 5px;
+         
+        }
+        
+        .errmessadmin{
+  color: red;
+  height: 5vh;
+  opacity: 0.5;
+  width: 17vw;
+  text-align: center;
+  border-radius: 0 0 15px 15px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
     </style>
- 
+   <div id="toggle">
+        <i class="inducator"></i>
+    </div>
     <form action="" method="POST" >
     <div class="congrat">
-        <h1>CONGRATULATION ! ! !</h1>
+        <h1>Admin Up! ! !</h1>
     </div>
-        <input type="text" name="pseudo"  autocomplete="off" placeholder="pseudo">
+  
+        <input class="motdepass" type="text" name="pseudo"  autocomplete="off" placeholder="pseudo">
         <br>
-        <input type="password" name="mdp" autocomplete="off" placeholder="mot de pass">
+ 
+     
+        <input class="motdepass" type="password" name="mdp" autocomplete="off" placeholder="mot de pass">
         <br><br>
-        <input type="submit"  name="valider">
+ 
+        
+        <input class="motdepass" type="submit"  name="valider">
+      
+        <section class="errmessadmin">
+<?php
+    if(isset($_GET['log_err'])){
+        $e = $_GET['log_err'];
+       if($e == 1){
+           echo 'Bad use or bad Mdp';
+       }
+    }
+?>
+</section>
 </form>
 </body>
+<script>
+        const body = document.querySelector('body');
+    
+        const toggle = document.getElementById('toggle');
+        toggle.onclick = function(){
+            toggle.classList.toggle('active');
+            body.classList.toggle('active');
+        }
+ 
+    </script>
 </html>
--- include/admin/indexad.php
+++ include/admin/indexad.php
@@ -11,13 +11,11 @@ if(!$_SESSION['mdp']){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=a, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/index.css">
+   
     <title>Page administration Gagnant</title>
 </head>
 <body>
-<div id="toggle">
-        <i class="inducator"></i>
-    </div>
+
     <style>
         *{
     margin: 0;
@@ -26,49 +24,15 @@ if(!$_SESSION['mdp']){
 }
 
 body{
-   transition: 0.5s;
+  
  background: white;  
  display: flex;
  justify-content:space-around;
         }
 
-body.active{
-    background: black; 
-}
+
 footer.active{
     color: red;
-}
-#toggle{
-    position: relative;
-    display: block;
-    width: 20px;
-    height: 20px;
-    border-radius: 16px;
-    background: white;
-    transition: 0.5s;
-    cursor: pointer;
-}
-#toggle.active{
-    background: black;
-    color: red;
-}
-#toggle .inducator{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 20px;
-    height: 20px;
-    background: linear-gradient(to bottom, #444,#222);
-    border-radius: 50%;
-    transform: scale(0.9);
-    transition: 0.5s;
-}
-#toggle.active .inducator{
-    left:110vh;
-    background: grey;
-    box-shadow: 0 8px 20px rgba(0,0,0,0.1),
-                inset 0 4px 4px rgba(255,255,255,1),
-                inset 0 -4px 4px rgba(255,255,255,1);
 }
      .contien{
       
@@ -116,13 +80,5 @@ footer a{
  <a href="adlogout.php">Se déconnecter</a>
  </footer>
 </body>
-<script>
-        const body = document.querySelector('body');
-    
-        const toggle = document.getElementById('toggle');
-        toggle.onclick = function(){
-            toggle.classList.toggle('active');
-            body.classList.toggle('active');
-        }
-    </script>
+
 </html>
