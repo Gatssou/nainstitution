00522bae66c02b3a16d81b32b504108c97e4ecbd
Gatssou <juliendu62155@hotmail.FR>
2021-12-16 16:58:09 +0100

    gf
    gf

--- index.php
+++ index.php
@@ -13,12 +13,19 @@ logged_only();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./sass/reliephpindex.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
    <script src="./js/jquery-3.6.0.min.js"></script>
     <title>Document</title>
 </head>
 <body>
     <header>
-
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap');
+</style> 
+<div class="titree">
+  <p class="n">N</p><p class="a">A</p><p class="i">I</p><p class="n">N</p><p class="v">V</p><p class="e">E</p><p class="i">N</p><p class="t">T</p><p class="u">U</p><p class="r">R</p><p class="a">A</p>
+</div>
     <!-- blipou -->
     <div class="toggle"><img src="./img/img_index/reticule.png" alt=""></div>
        <div class="menu">
@@ -38,13 +45,13 @@ logged_only();
         <a href="./personnagef.php"><ion-icon name="rocket-outline"></ion-icon></a>
         </li>
         <li style="--i:5;">
-        <a href="#"><ion-icon name="sparkles-outline"></ion-icon></a>
+        <a href=""><ion-icon name="sparkles-outline"></ion-icon></a>
         </li>
         <li style="--i:6;">
         <a href="#"><ion-icon name="sparkles-outline"></ion-icon></a>
         </li>
         <li style="--i:7;">
-        <a href="./php/disconnect.php"><ion-icon name="sparkles-outline"></ion-icon></a>
+        <a href="./php/disconnect.php"><img src="./img/index/logout.png" height="100px" width="100px" alt=""></a>
        
         </li>
     </div>
@@ -54,15 +61,7 @@ logged_only();
 
 </div>
   
-<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-    <script>
-        let toggle = document.querySelector('.toggle');
-        let menu = document.querySelector('.menu');
-        toggle.onclick = function(){ 
-            menu.classList.toggle('active')
-        }  
-    </script>
+
 </header>
 <div class="selectionner">
     <h1 class="selec"> Selectionner  </h1>
@@ -86,5 +85,14 @@ logged_only();
  <a href="./include/disconnect.php">Se déconnecter</a>
  </footer>
     </body>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script>
+        let toggle = document.querySelector('.toggle');
+        let menu = document.querySelector('.menu');
+        toggle.onclick = function(){ 
+            menu.classList.toggle('active')
+        }  
+    </script>
     <script src="./js/personnage.js"></script>
 </html>
