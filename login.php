ee6631d904ca9eed2efa09aae8ba19f7f9e8ad6e
Gatssou <87131331+Gatssou@users.noreply.github.com>
2022-03-02 08:55:16 +0100

    fd
    fdfd

--- login.php
+++ login.php
@@ -2,8 +2,8 @@
 if(isset($_GET['accepte-cookie'])){
     setcookie('accepte-cookie', 'true', time() + 365*24*3600);
     header('Location:./login.php')
-}
 ?>
+}
 <?php
 include './include/header.php';
 ?>
