9b73bd5fb5036aa72adff74cd40ae74c4ee1347b
Arecin0L <betrancourt.lonicera@gmail.com>
2022-03-02 08:55:57 +0100

    log

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
 if(isset($_GET['accepte-cookie'])){
     setcookie('accepte-cookie', 'true', time() + 365*24*3600);
-    header('Location:./pub.php')
-}
+}header('Location:./login.php')
+
 ?>
 <?php
 include './include/header.php';
