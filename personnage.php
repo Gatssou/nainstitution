43cd2b50cb8312721ee0835d56d9c143c061dc5b
Gatssou <87131331+Gatssou@users.noreply.github.com>
2022-03-04 11:26:03 +0100

    fgfgfgfgfgfgfgfg
    feuille d'abre

--- personnage.php
+++ personnage.php
@@ -9,19 +9,15 @@ logged_only();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/imagepersosnage/terre.gif" type="image">
     <link rel="stylesheet" href="./sass/personnages.css">
     <title>Document</title>
 </head>
 <body>
-    
-  
 <section>
     <div id="globe"> <img src="./img/imagepersosnage/terretrois.gif" alt="">
 </div>
-
-
     </section>
-    
     <div class="contien"> 
     <div class="menup ">
     <div class="maintexte">
