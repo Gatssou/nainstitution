e7d224d037252b70b8c2192b5248cfa3da98211e
Gatssou <juliendu62155@hotmail.FR>
2022-03-07 11:53:24 +0100

    jh
    jh

--- include/admin/connexion.php
+++ include/admin/connexion.php
@@ -25,7 +25,7 @@ $mdp_par_defaut = "A15H17luma";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="//img/imagepersosnage/terre.gif" type="image">
+    <link rel="shortcut icon" href="./img/terread.gif" type="image">
     <link rel="stylesheet" href="./css/index.css">
   
     <title>Document</title>
