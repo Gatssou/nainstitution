06fd71ae2f5ad9b9417c462a5d0bc78a607484db
Gatssou <juliendu62155@hotmail.FR>
2022-03-11 11:29:30 +0100

    hghg
    hghg

--- index.php
+++ index.php
@@ -287,7 +287,7 @@ logged_only();
 <script src="./js/menu.js"></script>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
 
 
 
