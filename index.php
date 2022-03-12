72b673840ec371799bf2ef872a689e255f7be2d2
Gatssou <87131331+Gatssou@users.noreply.github.com>
2022-03-12 16:24:24 +0100

    kjkj
    kjkj

--- index.php
+++ index.php
@@ -23,36 +23,22 @@ logged_only();
 
 <body>
     <div class="para">
-        <div class="menu ">
-            <div class="toggle nav">
-                <img src="./img/index/gouvernail.png" alt="">
-            </div>
-
-            <li class="icon" style="--i: 0">
-                <a href="./pub.php">
-                    <ion-icon name="home-outline"></ion-icon>
-                </a>
-            </li>
-            </a>
-            <li class="icon" style="--i: 1">
-                <a href="./include/disconnect.php">
-                    <ion-icon name="close-circle-outline"></ion-icon>
-                </a>
-            </li>
-
-            <li class="icon" style="--i: 2">
-                <a href="">
-                    <ion-icon name="arrow-redo-outline"></ion-icon>
-                </a>
-            </li>
-        </div>
-        <div class="menu" onclick="this.classList.toggle('open')">
-            <div class="button"></div>
-            <div class="button"></div>
-            <div class="button"></div>
-        </div>
-
+    <div id="menud">
+				<div id="div1" tabindex="0">   <!-- tabindex ="0" 1 2 4 5 etc indique si l'élément peut capturer le focus et si c'est le cas, dans quel ordre il le capture lors de la navigation au clavier généralement à l'aide de la touche Tab. Si plusieurs éléments partagent la même valeur d'attribut tabindex, leur ordre sera calculé en fonction de leur position dans le document.  -->
+					<div class="imgg">
+                    <img src="./img/index/gouvernail.png"  alt="">
+                    </div> 
+					<ul>
+				 	<li>Home</li>
+						<li>truc deux</li>
+						<li>truc trois</li>	
+					</ul>
+				</div>
+                </div>
         <section id="pic_1" class="sec">
+        <nav>
+      
+    </nav>
             <div class="av1">
                 <div class="ttl">Mike Horn</div>
                 <div id="avs">
